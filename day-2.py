a16ebbcdaa0c611a1e8ccffac4b99e2f6badb0a8
yeswitha pirakala <106010173+yeswithapirakala@users.noreply.github.com>
2025-08-11 22:55:58 +0530

    Add files via upload

---
+++ day-2.py
@@ -0,0 +1,68 @@
+#task 3
+
+"""The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+
+1. The first line contains the sum of the two numbers.
+
+2. The second line contains the difference of the two numbers (first -second).
+
+3. The third line contains the product of the two numbers.
+
+Example
+
+a=3
+
+b=5
+
+Print the following:
+
+8
+
+-2
+
+15
+
+Input Format
+
+The first line contains the first integer, a.
+
+The second line contains the second integer, b.
+
+Constraints
+
+1 ≤ a ≤ 1010
+
+1 ≤ b ≤1010
+
+Outrut Format
+Print the three lines as explained above.
+
+Sample Input O
+
+3
+
+2
+
+Sample Output O
+
+5
+
+1
+
+6
+
+Explanation O
+
+3+25
+
+3-21
+
+3*26"""
+
+#code
+
+a=int(input())
+b=int(input())
+print(a+b)
+print(a-b)
+print(a*b)
---
+++ day-3.py
@@ -0,0 +1,62 @@
+#task 2
+
+"""Given an integer, n. perform the following conditional actions:
+
+If n is odd. print Weird
+
+If n is even and in the inclusive range of 2 to 5. print Not Weird
+
+If n is even and in the inclusive range of 6 to 20. print Weird
+
+If n is even and greater than 20. print Not Weird
+
+Input Format
+
+A single line containing a positive integer, n.
+
+Constraints
+
+1≤ n ≤100
+
+Output Format
+
+Print Weird if the number is weird. Otherwise, print Not Weird.
+
+Sample Input O
+
+3
+
+Sample Output O
+
+Weird
+
+Explanation O
+
+N=3
+
+n is odd and odd numbers are weird, so print Weird.
+
+Sample Input 1
+
+24
+
+Sample Output 1
+
+Not Weird
+
+Explanation 1
+
+n = 24
+
+n > 20 and n is even, so it is not weird."""
+
+#code
+n=int(input())
+if n%2!=0:
+    print("Weird")
+elif n%2==0 and 2<=n<=5:
+    print("Not Weird")
+elif n%2==0 and 6<=n<=20:
+    print("Weird")
+elif n%2==0 and n>20:
+    print("Not Weird")
